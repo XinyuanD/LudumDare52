f80dc7ebd384ec59984ecc5ccc5d46a547a538c8
XinyuanD <xinyuand6@gmail.com>
2023-01-06 23:38:49 -0800

    Fixed player movement
    Fixed player movement, made the code cleaner, put in sprites, and reorganized folders

--- LudumDare52Game/Assets/Scripts/PlayerController.cs
+++ LudumDare52Game/Assets/Scripts/PlayerController.cs
@@ -5,53 +5,33 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    Vector2 movementInput;
-    Rigidbody2D rb;
-    public float moveSpeed = 1f;
-    public float collisionOffset = 0.05f;
-    public ContactFilter2D movementFilter;
-    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+    // components
+    private Rigidbody2D rb;
+    private BoxCollider2D collider2d;
+
+    // variables
+    [SerializeField] private float moveSpeed = 5f;
+    private Vector2 moveInput;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        collider2d = GetComponent<BoxCollider2D>();
     }
 
-    void Update()
-    {
-        
-    }
-    
     void FixedUpdate()
     {
-        if(movementInput != Vector2.zero){
-            bool success = TryMove(movementInput);
-
-            if(!success) {
-                success = TryMove(new Vector2(movementInput.x, 0));
-
-                if(!success) {
-                    success = TryMove(new Vector2(0, movementInput.y));
-                }
-            }
-        }
+        Move();
     }
 
-    private bool TryMove(Vector2 direction){
-        int count = rb.Cast(
-            direction,
-            movementFilter,
-            castCollisions,
-            moveSpeed * Time.fixedDeltaTime + collisionOffset);
-        if(count == 0){
-            rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
-            return true;
-        } else {
-            return false;
-        }
-    }
-    void OnMove(InputValue movementValue) 
+    private void Move()
     {
-        movementInput = movementValue.Get<Vector2>();
+        Vector2 velocity = moveInput * moveSpeed;
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+    }
+    
+    private void OnMove(InputValue value) 
+    {
+        moveInput = value.Get<Vector2>();
     }
 }
